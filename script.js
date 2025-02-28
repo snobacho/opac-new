4c1510ebbbd8f18ba4bd4912c42badfb9ed23dd8
Giorgi Chelidze <122386771+snobacho@users.noreply.github.com>
2025-02-28 11:43:19 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 4,
+        },
+    },
+});
